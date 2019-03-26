74a5dd19e47b6f911cc7ec6fc232f6d57e15fb29
icehongssii <icehongssii@gmail.com>
2019-03-26 19:12:49 +0900

    import error mpl_finance and csv

--- source/main-gen.py
+++ source/main-gen.py
@@ -20,7 +20,7 @@ iter_num = int(sys.argv[3])
 def ten_three_img_gen():
     counter = 0
     for f in tqdm(files):
-        if('.txt' not in f): continue
+        if('.csv' not in f): continue
         if((len(sys.argv) == 3) and (counter > iter_num)): break
         os.system('python3 ./10-3-gen.py '+sys.argv[1]+'/'+f)
         print('generating: %s, iteration: %d' % (f, counter))
@@ -29,7 +29,7 @@ def ten_three_img_gen():
 def twenty_three_matrix_gen():
     counter = 0
     for f in tqdm(files):
-        if('.txt' not in f): continue
+        if('.csv' not in f): continue
         if(counter > iter_num): break
         os.system('python3 ./matrix_gen.py '+sys.argv[2]+'/'+f+' '+sys.argv[4]) 
         # argv3 output root dir
